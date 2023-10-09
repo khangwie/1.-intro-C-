4d2c01d28e3946f1d3cf71b039ebf3e927cd32bd
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-09 10:42:30 +0200

    Add project files.

---
+++ 1. intro C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.intro_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine(); // to freeze the console so that it does not just flash
+        }
+    }
+}
